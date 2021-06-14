c3f9d1e1d8f22a63a678e2121a14dc865be48d32
Mark Leitheiser <mark.leitheiser@snhu.edu>
2021-06-14 14:55:05 -0500

    Added the image upload capability to firebase storage

--- app/src/main/java/mleith785/cs499/firebasedb/Image_test.java
+++ app/src/main/java/mleith785/cs499/firebasedb/Image_test.java
@@ -3,22 +3,36 @@ package mleith785.cs499.firebasedb;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.storage.FirebaseStorage;
 
+
+
 //!!!!this is the import you need!
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.ByteArrayOutputStream;
+import java.util.UUID;
 
 import static android.content.ContentValues.TAG;
 
@@ -78,11 +92,28 @@ public class Image_test extends AppCompatActivity
         CampDetailAvgRatingGui.setText(String.valueOf(DisHereCampsite.AvgRating));
         CityText.setText(DisHereCampsite.CampCity);
 
-
+        //NEED THIS AT start for image stuff
         storage = FirebaseStorage.getInstance();
         storageReference = storage.getReference();
 
-        String path = "temp";
+        String image_paths[] =
+        {
+                "campsite_images/Clearly Lake.jpeg",
+                "campsite_images/Como_park.jpeg",
+                "campsite_images/Lebanon_hills_regional.jpeg",
+                "campsite_images/Minneapolis_sw_koa.jpeg",
+                "campsite_images/Phalen_regional.jpeg",
+                "campsite_images/afton_state_park.jpeg",
+                "campsite_images/baker_campground.jpeg",
+                "campsite_images/father_hennepin_bluff.jpeg",
+                "campsite_images/lake_auburn_campground.jpeg",
+                "campsite_images/minnehaha_regional.jpeg",
+                "campsite_images/town_country_rv.jpeg",
+                "campsite_images/voyagers_campground.jpeg"
+        };
+        int index = 0;
+
+        String path = image_paths[index];
         setImage(path);
 
     }
@@ -94,7 +125,7 @@ public class Image_test extends AppCompatActivity
         //Taken from here
         //https://www.youtube.com/watch?v=iTSFJwsKPns
 
-        StorageReference pathReference = storageReference.child("campsite_images/Como_park.jpeg");
+        StorageReference pathReference = storageReference.child(path);
         long MAXBYTES = 1024*1024;
         pathReference.getBytes(MAXBYTES).addOnSuccessListener(new OnSuccessListener<byte[]>()
         {
@@ -113,5 +144,58 @@ public class Image_test extends AppCompatActivity
                 Log.w(TAG, "failed to read picture");
             }
         });
+    }
+
+
+    public void UploadBtn(View view)
+    {
+        final ProgressDialog pd = new ProgressDialog(this);
+        //progress bar and other info from
+        //https://www.youtube.com/watch?v=CQ5qcJetYAI
+        pd.setTitle("Uploading Image...");
+        pd.show();
+
+        Bitmap BM;
+        BM = ((BitmapDrawable)CampImageGui.getDrawable()).getBitmap();
+
+        final String randomKey = UUID.randomUUID().toString();
+        final String base_path = "campsite_images/";
+        String path_to_write = base_path + randomKey+".jpeg";
+
+        StorageReference riversRefRef = storageReference.child(path_to_write);
+
+        //Taken from
+        //https://firebase.google.com/docs/storage/android/upload-files
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        BM.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+        UploadTask uploadTask = riversRefRef.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener()
+        {
+            @Override
+            public void onFailure(@NonNull @NotNull Exception e)
+            {
+                pd.dismiss();
+                Log.w(TAG, "failed to upload picture");
+                Toast.makeText(getApplicationContext(),"Failed to upload", Toast.LENGTH_LONG).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>()
+        {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot)
+            {
+                pd.dismiss();
+                Snackbar.make(findViewById(android.R.id.content),"Image Uploaded.",Snackbar.LENGTH_LONG).show();
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>()
+        {
+            @Override
+            public void onProgress(@NonNull @NotNull UploadTask.TaskSnapshot snapshot)
+            {
+                double progressPercent = (100.00* snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                pd.setMessage("Percentage: " + (int)progressPercent+"%");
+            }
+        });
+
     }
 }
